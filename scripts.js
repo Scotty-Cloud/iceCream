ad7d12ecbc873f6497402398b52c4d7b03ca8c69
scott <scottysora@gmail.com>
2024-05-03 11:28:36 -0400

    add conditions to check prices

--- scripts.js
+++ scripts.js
@@ -1,9 +1,41 @@
-window.onload = function init(){
-    const orderSubmitBtn = document.getElementById("orderSubmit");
-    const coneRadioOption = document.getElementById("coneOption");
-    const cupRadioOption = document.getElementById("cupOption");
-}
+window.onload = function init() {
+  const orderSubmitBtn = document.getElementById("orderSubmit");
+  const coneRadioOption = document.getElementById("coneOption");
+  const cupRadioOption = document.getElementById("cupOption");
+};
 
-function totalPrice(){
-    
+function totalPrice() {
+  const scoopInput = document.getElementById("scoopInput");
+  const scoopAmount = parseInt(scoopInput.value);
+  const coneRadioOption = document.getElementById("coneOption");
+  const cupRadioOption = document.getElementById("cupOption");
+  const sprinkleBox = document.getElementById("sprinkleBox");
+  const whippedcreamBox = document.getElementById("whippedcreamBox");
+  const hotfudgeBox = document.getElementById("hotfudgeBox");
+  const cherryBox = document.getElementById("cherryBox");
+
+  let basePrice = 2.25;
+  let toppingsPrice = 0;
+
+  if (coneRadioOption.checked) {
+    basePrice = scoopAmount * 2.5;
+  } else if (cupRadioOption.checked) {
+    basePrice = scoopAmount * 3.0;
+  }
+  if (sprinkleBox.checked) {
+    toppingsPrice += 0.5;
+  }
+  if (whippedcreamBox.checked) {
+    toppingsPrice += 0.25;
+  }
+  if (hotfudgeBox.checked) {
+    toppingsPrice += 1.25;
+  }
+  if (cherryBox.checked) {
+    toppingsPrice += 0.25;
+  }
+
+  const taxRate = 0.08;
+  const taxAmount = (basePrice + toppingsPrice) * taxRate;
+
 }
