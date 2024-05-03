c250e2f1cbd4daa39fb7e7fb9eea86018a608e48
scott <scottysora@gmail.com>
2024-05-03 11:43:07 -0400

    complete

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 window.onload = function init() {
   const orderSubmitBtn = document.getElementById("orderSubmit");
+  orderSubmitBtn.addEventListener("click", totalPrice);
   const coneRadioOption = document.getElementById("coneOption");
   const cupRadioOption = document.getElementById("cupOption");
 };
@@ -14,28 +15,34 @@ function totalPrice() {
   const hotfudgeBox = document.getElementById("hotfudgeBox");
   const cherryBox = document.getElementById("cherryBox");
 
-  let basePrice = 2.25;
-  let toppingsPrice = 0;
-
-  if (coneRadioOption.checked) {
-    basePrice = scoopAmount * 2.5;
-  } else if (cupRadioOption.checked) {
+  let basePrice = 0;
+  if (document.getElementById("coneOption").checked) {
+    basePrice = scoopAmount * 2.25;
+  } else if (document.getElementById("cupOption").checked) {
     basePrice = scoopAmount * 3.0;
   }
-  if (sprinkleBox.checked) {
-    toppingsPrice += 0.5;
+
+  let toppingsPrice = 0;
+  if (document.getElementById("sprinkleBox").checked) {
+    toppingsPrice += 0.50;
   }
-  if (whippedcreamBox.checked) {
+  if (document.getElementById("whippedcreamBox").checked) {
     toppingsPrice += 0.25;
   }
-  if (hotfudgeBox.checked) {
+  if (document.getElementById("hotfudgeBox").checked) {
     toppingsPrice += 1.25;
   }
-  if (cherryBox.checked) {
+  if (document.getElementById("cherryBox").checked) {
     toppingsPrice += 0.25;
+  } else {
+    toppingsPrice = 0;
   }
 
   const taxRate = 0.08;
   const taxAmount = (basePrice + toppingsPrice) * taxRate;
+  const totalDue = basePrice + toppingsPrice + taxAmount;
 
+  document.getElementById("basePriceTotal").innerText = `$${basePrice.toFixed(2)}`;
+  document.getElementById("taxTotal").innerText = `$${taxAmount.toFixed(2)}`;
+  document.getElementById("entireTotalDue").innerText = `$${totalDue.toFixed(2)}`;
 }
