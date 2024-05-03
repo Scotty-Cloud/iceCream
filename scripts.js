0f3caf41a25b933e4616075e51ec92ba7c3d685c
scott <scottysora@gmail.com>
2024-05-03 10:57:15 -0400

    create init function

---
+++ scripts.js
@@ -0,0 +1,9 @@
+window.onload = function init(){
+    const orderSubmitBtn = document.getElementById("orderSubmit");
+    const coneRadioOption = document.getElementById("coneOption");
+    const cupRadioOption = document.getElementById("cupOption");
+}
+
+function totalPrice(){
+    
+}
