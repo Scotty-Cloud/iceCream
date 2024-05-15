32e62998832194525e612ed26815fc4a6cb5af21
scott <scottysora@gmail.com>
2024-05-15 17:36:59 -0400

    fully functional

--- scripts.js
+++ scripts.js
@@ -1,45 +1,74 @@
 window.onload = function init() {
-    const orderSubmitBtn = document.getElementById("orderSubmit");
-    orderSubmitBtn.addEventListener("click", totalPrice);
-  };
-  
-  function totalPrice() {
-    const coneRadioOption = document.getElementById("coneOption");
-    const cupRadioOption = document.getElementById("cupOption");
-    const scoopInput = document.getElementById("scoopInput");
-    const scoopAmount = parseInt(scoopInput.value);
-    const sprinkleBox = document.getElementById("sprinkleBox");
-    const whippedcreamBox = document.getElementById("whippedcreamBox");
-    const hotfudgeBox = document.getElementById("hotfudgeBox");
-    const cherryBox = document.getElementById("cherryBox");
-    const basePriceTotal = document.getElementById("basePriceTotal");
-    const taxTotal = document.getElementById("taxTotal");
-    const entireTotalDue = document.getElementById("entireTotalDue");
-  
-    let aScoop = 2.25 + (scoopAmount - 1) * 1.25;
-  
-    let toppingsPrice = 0;
-    if (cupRadioOption.checked) {
-      if (sprinkleBox.checked) {
-        toppingsPrice += 0.50;
-      }
-      if (whippedcreamBox.checked) {
-        toppingsPrice += 0.25;
-      }
-      if (hotfudgeBox.checked) {
-        toppingsPrice += 1.25;
-      }
-      if (cherryBox.checked) {
-        toppingsPrice += 0.25;
-      }
-    }
-  
-    let base = aScoop + toppingsPrice;
-    basePriceTotal.innerText = base.toFixed(2);
-  
-    let tax = base * 0.08;
-    taxTotal.innerText = tax.toFixed(2);
-  
-    let total = tax + base;
-    entireTotalDue.innerText = total.toFixed(2);
+  const orderSubmit = document.getElementById("orderSubmit");
+  const cupOption = document.getElementById("cupOption");
+  const coneOption = document.getElementById("coneOption");
+
+  hideToppings();
+
+  cupOption.onclick = hideToppings;
+  coneOption.onclick = hideToppings;
+
+  orderSubmit.onclick = totalPayment;
+};
+
+function hideToppings() {
+  const cupOption = document.getElementById("cupOption").checked;
+  const toppingsCheckbox = document.getElementById("toppingsCheckbox");
+
+  if (cupOption) {
+    toppingsCheckbox.style.display = "block";
+  } else {
+    toppingsCheckbox.style.display = "none";
   }
+}
+
+function totalPayment() {
+  const numberOfScoop = Number(document.getElementById("scoopAmount").value);
+
+  const coneOption = document.getElementById("coneOption").checked;
+  const cupOption = document.getElementById("cupOption").checked;
+
+  const sprinkleBox = document.getElementById("sprinkleBox").checked;
+  const whipcreamBox = document.getElementById("whipcreamBox").checked;
+  const hotfudgeBox = document.getElementById("hotfudgeBox").checked;
+  const cherryBox = document.getElementById("cherryBox").checked;
+
+  const basePriceTotal = document.getElementById("basePriceTotal");
+  const taxAppllied = document.getElementById("taxAppllied");
+  const entireTotal = document.getElementById("entireTotal");
+
+  let oneScoop = 2.25;
+
+  if (numberOfScoop > 1) {
+    oneScoop += 1.25 * (numberOfScoop - 1);
+  }
+
+  toppingsTotal = 0;
+  if (cupOption) {
+    if (sprinkleBox) {
+      toppingsTotal += 0.5;
+    }
+    if (whipcreamBox) {
+      toppingsTotal += 0.25;
+    }
+    if (hotfudgeBox) {
+      toppingsTotal += 1.25;
+    }
+    if (cherryBox) {
+      toppingsTotal += 0.25;
+    }
+  } else {
+    toppingsTotal = 0;
+  }
+
+  let basePrice = oneScoop + toppingsTotal;
+  basePriceTotal.innerText = basePrice;
+
+  let taxAmount = basePrice * 0.08;
+
+  taxAppllied.innerText = taxAmount;
+
+  let totalDue = taxAmount + basePrice;
+
+  entireTotal.innerText = totalDue;
+}
